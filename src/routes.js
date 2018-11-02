1d47bdcf05a76399570df70ad7e0ad2a96e068d0
Snehal Kale <snehal.kale@eternussolutions.com>
2018-11-02 18:09:46 +0530

    solve bug #14652

--- src/routes.js
+++ src/routes.js
@@ -131,8 +131,8 @@ const routes = [
     component: ApplicationContacts
   },
   { path: "/master/crops", name: "Crops Data", component: CropsData },
-  { path: "/settings", name: "Settings", component: Settings },
-  { path: "/ChangePassword", name: "ChangePassword", component: ChangePassword },
+  { path: "/settings/Myprofile", name: "Settings", component: Settings },
+  { path: "/settings/ChangePassword", name: "ChangePassword", component: ChangePassword },
   { path: "/beneficiary/inactiveUser", name: "InactiveUser", component: InactiveUser },
   { path: "/operationalUser", name: "OperationalUser", component: OperationalUser },
   { path: "/operationalUser/operationalUserList", name: "OperationalUserList", component: OperationalUser },
